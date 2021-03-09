8c0c88819b9d86507204e516b779bfacafa41b52
noahhelton98 <noahhelton98@gmail.com>
2021-03-08 21:24:18 -0500

    made password function and input textbox to define length

---
+++ script.js
@@ -0,0 +1,142 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+function generatePassword(length, lower, upper, number, special){
+  if (length < 8 || length > 128){
+    alert('Error: Please enter a length between 8 and 128');
+    return  password = '';
+  }
+  if (lower == false && upper == false && special == false && number == false){
+    alert('Error: Please enter valid criteria');
+    return  password = '';
+
+  }
+  var password = '';
+  var charArray = [];
+  if (lower == true){
+    charArray = charArray.concat(lowerCasedCharacters);
+  }
+  if(upper == true){
+    charArray = charArray.concat(upperCasedCharacters);
+  }
+  if(number == true){
+    charArray = charArray.concat(numericCharacters)
+  }
+  if(special == true){
+    charArray = charArray.concat(specialCharacters)
+  }
+  var passwordArray = [];
+  for (var i = 0; i < length; i++){
+    passwordArray = passwordArray.concat(charArray[Math.floor(Math.random() * charArray.length)]);
+  }
+  password = passwordArray.join('')
+  return password;
+}
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var x = parseInt(document.getElementById("myNumber").value);
+
+  var lowerBool = document.getElementById("lower");
+  var upperBool = document.getElementById("upper");
+  var numberBool = document.getElementById("numbers");
+  var specialBool = document.getElementById("special")
+
+  var password = generatePassword(x, true, true, true, true);
+  document.getElementById('password').value = password
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
